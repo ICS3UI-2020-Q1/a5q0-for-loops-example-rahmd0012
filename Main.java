0a15b34ba336772cde3460009bfdd5be86766175
rahmd0012 <rahmd0012@wrdsb.ca>
2020-10-07 12:49:44 +0000

    for loop example

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * The for loop example
+ * @author Daniel Rahmani
  */
 public class Main {
 
@@ -9,7 +10,24 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // create the count variable
+    int count = 1;
+    // create the while loop to count at 10
+    while(count<=10){
+      // loop action: print number to the console
+      System.out.println(count);
+      // increase the counter
+      count = count + 1;
+    }
+
+    // do the same thing with for loop
+    for(int count2 = 0; count2 < 10; count2++){
+      // loop action: print number
+      System.out.println("Loop " + (count2+1));
+    }
     
   }
 }
